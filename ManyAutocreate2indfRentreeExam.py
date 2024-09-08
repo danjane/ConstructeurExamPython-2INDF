5242d6e695327d7519872e7a5ff39f07a44a2339
danjane <jdtane@gmail.com>
2024-09-08 21:51:49 +0200

    Footer and some info text when creating many of them

--- ManyAutocreate2indfRentreeExam.py
+++ ManyAutocreate2indfRentreeExam.py
@@ -1,11 +1,12 @@
 import Autocreate2indfRentreeExam
 import pdfrw
 
-n_copies = 2
+n_copies = 18
 
 writer = pdfrw.PdfWriter()
 
-for _ in range(n_copies):
+for i in range(n_copies):
+    print(f'Creating test {i} of {n_copies}...')
     pdf_file = Autocreate2indfRentreeExam.generate_exam()
     writer.addpages(pdfrw.PdfReader(pdf_file).pages)
 
